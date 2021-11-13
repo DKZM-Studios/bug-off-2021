a9044d7d99c1928f194332c1e9525de1e1231891
Dynamic6448 <memonfaw@studentmail.conroeisd.net>
2021-11-13 16:46:01 -0600

    Added rotating coins

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -6,8 +6,10 @@ public class Coin : MonoBehaviour
 {
     public float waveHeight;
     public float waveSpeed;
+    public float rotationSpeed;
     void Update()
     {
+        transform.Rotate(0f, rotationSpeed * Time.deltaTime, 0f);
         transform.localPosition = new Vector2(transform.localPosition.x, transform.localPosition.y + (waveHeight * Mathf.Sin(waveSpeed * Time.time)) * 0.0001f);
     }
 }
